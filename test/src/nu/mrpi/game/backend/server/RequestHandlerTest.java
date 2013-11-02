8ef5e42c7a0b5e3fcb5d327a8516b199770349ee
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2013-11-02 13:48:20 +0100

    Added some more tests for request handler

--- test/src/nu/mrpi/game/backend/server/RequestHandlerTest.java
+++ test/src/nu/mrpi/game/backend/server/RequestHandlerTest.java
@@ -5,14 +5,17 @@ import nu.mrpi.game.backend.server.modules.WebModule;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
+import java.util.Arrays;
 import java.util.Collections;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Pierre Ingmansson (pierre@ingmansson.com)
@@ -37,8 +40,61 @@ public class RequestHandlerTest {
 
         requestHandler.handle(request);
 
+        verifyNotFoundSent();
+    }
+
+    @Test
+    public void delegatesToAcceptingModule() throws Exception {
+        WebModule acceptingModule = createAcceptingModule();
+
+        RequestHandler requestHandler = new RequestHandler(Arrays.asList(acceptingModule));
+
+        requestHandler.handle(request);
+
+        verify(acceptingModule).handleRequest(request);
+    }
+
+    @Test
+    public void delegatesToFirstAcceptingModule() throws Exception {
+        WebModule notAcceptingModule = createNotAcceptingModule();
+        WebModule acceptingModule = createAcceptingModule();
+
+        RequestHandler requestHandler = new RequestHandler(Arrays.asList(notAcceptingModule, acceptingModule));
+
+        requestHandler.handle(request);
+
+        verify(acceptingModule).handleRequest(request);
+    }
+
+    @Test
+    public void fallbackOnDefaultWhenNoModuleAccepted() throws Exception {
+        WebModule notAcceptingModule = createNotAcceptingModule();
+        WebModule otherNotAcceptingModule = createNotAcceptingModule();
+
+        RequestHandler requestHandler = new RequestHandler(Arrays.asList(notAcceptingModule, otherNotAcceptingModule));
+
+        requestHandler.handle(request);
+
+        verifyNotFoundSent();
+    }
+
+    private void verifyNotFoundSent() throws IOException {
         verify(request).sendResponseHeaders(404, NOT_FOUND.length());
         verify(outputStream).write(NOT_FOUND.getBytes());
         verify(outputStream).close();
+    }
+
+    private WebModule createAcceptingModule() {
+        return createModule(true);
+    }
+
+    private WebModule createNotAcceptingModule() {
+        return createModule(false);
+    }
+
+    private WebModule createModule(boolean accepting) {
+        WebModule webModule = mock(WebModule.class);
+        when(webModule.handlesPath(Matchers.<URI>anyObject())).thenReturn(accepting);
+        return webModule;
     }
 }
