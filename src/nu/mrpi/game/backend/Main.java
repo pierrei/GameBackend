cbd89ac02e17f75b259506204bc0e0cd3a60b877
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2013-11-02 11:57:25 +0100

    First version of Main

---
+++ src/nu/mrpi/game/backend/Main.java
@@ -0,0 +1,14 @@
+package nu.mrpi.game.backend;
+
+/**
+ * @author Pierre Ingmansson (pierre@ingmansson.com)
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main().startServer();
+    }
+
+    private void startServer() {
+        System.out.println("Starting server..");
+    }
+}
