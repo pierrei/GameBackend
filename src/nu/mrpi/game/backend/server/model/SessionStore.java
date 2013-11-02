d57751e8832d49770df5f927e79124e4a45d2a8a
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2013-11-02 14:14:31 +0100

    Created a simple session store

---
+++ src/nu/mrpi/game/backend/server/model/SessionStore.java
@@ -0,0 +1,10 @@
+package nu.mrpi.game.backend.server.model;
+
+/**
+ * @author Pierre Ingmansson (pierre@ingmansson.com)
+ */
+public class SessionStore {
+    public String createSession(int userId) {
+        return "ID";
+    }
+}
