7b2d38632d78398cfe6b1e7989219dda7e1cc9be
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2013-11-02 18:43:23 +0100

    Added some more tests

--- test/src/nu/mrpi/game/backend/server/model/LevelScoreBoardTest.java
+++ test/src/nu/mrpi/game/backend/server/model/LevelScoreBoardTest.java
@@ -3,6 +3,8 @@ package nu.mrpi.game.backend.server.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -22,5 +24,27 @@ public class LevelScoreBoardTest {
         levelScoreBoard.updateUserScore(1, 20);
 
         assertEquals(1, levelScoreBoard.getLevelScoreBoardAsMap().size());
+    }
+
+    @Test
+    public void updateUserScoreKeepsTheHighestScoreForOneUser() throws Exception {
+        levelScoreBoard.updateUserScore(1, 10);
+        levelScoreBoard.updateUserScore(1, 20);
+        levelScoreBoard.updateUserScore(1, 11);
+
+        Map<Integer,Integer> scores = levelScoreBoard.getLevelScoreBoardAsMap();
+        assertEquals(1, scores.size());
+        assertEquals((Integer) 20, scores.get(1));
+    }
+
+    @Test
+    public void updateUserScoreKeepsScoresForMoreThanOneUser() throws Exception {
+        levelScoreBoard.updateUserScore(1, 10);
+        levelScoreBoard.updateUserScore(2, 20);
+
+        Map<Integer,Integer> scores = levelScoreBoard.getLevelScoreBoardAsMap();
+        assertEquals(2, scores.size());
+        assertEquals((Integer) 10, scores.get(1));
+        assertEquals((Integer) 20, scores.get(2));
     }
 }
