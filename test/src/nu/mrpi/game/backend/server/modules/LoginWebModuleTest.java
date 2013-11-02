de34a7ca9c8ec8c9c3345ddc612ec8651586388e
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2013-11-02 13:57:41 +0100

    Added some more tests for login

--- test/src/nu/mrpi/game/backend/server/modules/LoginWebModuleTest.java
+++ test/src/nu/mrpi/game/backend/server/modules/LoginWebModuleTest.java
@@ -1,14 +1,20 @@
 package nu.mrpi.game.backend.server.modules;
 
+import com.sun.net.httpserver.HttpExchange;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URI;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Pierre Ingmansson (pierre@ingmansson.com)
@@ -18,9 +24,17 @@ public class LoginWebModuleTest {
 
     private LoginWebModule loginModule;
 
+    @Mock
+    private HttpExchange request;
+
+    @Mock
+    private OutputStream outputStream;
+
+
     @Before
     public void setUp() throws Exception {
         loginModule = new LoginWebModule();
+        when(request.getResponseBody()).thenReturn(outputStream);
     }
 
     @Test
@@ -46,5 +60,18 @@ public class LoginWebModuleTest {
     @Test
     public void doesNotHandleLoginUrlWithTrailingSlash() throws Exception {
         assertFalse(loginModule.handlesPath(new URI("/10/login/")));
+    }
+
+    @Test
+    public void handleRequestReturnsOk() throws Exception {
+        loginModule.handleRequest(request);
+
+        verifyOkSent("Logged in!");
+    }
+
+    private void verifyOkSent(String responseBody) throws IOException {
+        verify(request).sendResponseHeaders(200, responseBody.length());
+        verify(outputStream).write(responseBody.getBytes());
+        verify(outputStream).close();
     }
 }
