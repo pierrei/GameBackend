343858efda6f8f99bee6928d5f4f5315cedfdbda
Pierre Ingmansson <pierre.ingmansson@prezi.com>
2013-11-03 15:02:54 +0100

    Made settings not instantiable

--- src/nu/mrpi/game/backend/server/Settings.java
+++ src/nu/mrpi/game/backend/server/Settings.java
@@ -10,4 +10,7 @@ public class Settings {
     public static final int SESSION_ID_LENGTH = 8;
     public static final int SECONDS_TO_WAIT_BEFORE_STOPPING = 5;
     public static final int MAX_USER_ID = ((int) Math.pow(2.0, 31.0)); // unsigned 31 bit integer
+
+    private Settings() {
+    }
 }
